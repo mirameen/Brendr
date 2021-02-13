b480c6db10789a0e27740be7a271b5cc6398ae0a
Mir Ameen <mirameen84@gmail.com>
2021-02-14 00:37:49 +0530

    Some changes to setup

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const createError = require('http-errors');
+const bodyParser = require('body-parser');
 const path = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://mirameen84:VXIg8sFOpVnhZ4mB@mongouploads.ppu8t.mongodb.net/brendr?retryWrites=true&w=majority',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Connected to mongodb!");
+}).catch(err => {
+  console.log(err);
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
