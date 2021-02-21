981d43d5da6cac08abe91114be14db571f2ad86c
Nipun Wahi <nipunwahi@pop-os.localdomain>
2021-02-21 11:01:24 +0530

    added models and routes for items

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const itemRouter  = require('./routes/item');
 
 const app = express();
 
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/items',itemRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -48,5 +50,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.listen(3000, () => {
+  console.log('App listening on port 3000!');
+});
 module.exports = app;
