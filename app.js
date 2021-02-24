601d3d2759fbbd61a289845fd0d480de041193bd
Mir Ameen <mirameen84@gmail.com>
2021-02-24 10:35:56 +0530

    port change

--- app.js
+++ app.js
@@ -50,7 +50,4 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-app.listen(3000, () => {
-  console.log('App listening on port 3000!');
-});
 module.exports = app;
