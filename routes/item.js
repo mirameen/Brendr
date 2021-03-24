a9c151cd7998da0daa7e7b242742ad109054e532
sarthaksheoran <sarthaksheoran@gmail.com>
2021-03-24 19:01:25 +0530

    succesfully implemented item get post requests

--- routes/item.js
+++ routes/item.js
@@ -4,13 +4,13 @@ var Item = require("../models/Item")
 var User = require("../models/User")
 
 router.get('/', function(req, res, next) {
-    Item.find({}).populate(userID).exec(function(err,items){
+    Item.find({}).populate('userID').exec(function(err,items){
         if(err) console.log(err);
         else res.send(items);
     });
 });
 
-router.post("/",function(req,res,next){
+router.post("/register",function(req,res,next){
 
     var name  = req.body.name;
     var image = req.body.image;
@@ -45,7 +45,7 @@ router.post("/",function(req,res,next){
                 console.log(err);
             }
         console.log('User database updated ' + user.email);
-        res.send('Item Succesfully saved and user history updated');
+        res.send( user);
         });
         
       });
