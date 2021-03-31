67ba16cf1edd8c2452780cb1365abc4d9bb425a0
Mir Ameen <mirameen84@gmail.com>
2021-03-31 16:56:46 +0530

    deployment changes

--- app.js
+++ app.js
@@ -40,7 +40,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/items',itemRouter);
 
